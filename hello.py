3e9e036500fabd9405dc3a21688553b628a9589c
itibia <itibia@itibia.com>
2018-10-23 10:30:53 +0800

    重定向、消息闪现

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -19,12 +19,14 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	name=None
 	form=NameForm()
 	if form.validate_on_submit():
-		name=form.name.data
-		form.name.data=''
-	return render_template('index.html',current_time=datetime.utcnow(),form=form,name=name)
+		old_name=session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name']=form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html',current_time=datetime.utcnow(),form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
